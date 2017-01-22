612ece75e8cdbeb5b48977b9c3e9eddef25ec930
Anne Remacle <anne.m.remacle@gmail.com>
2017-01-22 18:05:26 +0100

    Integrating newsletter form

--- dev/wp-content/themes/csi/footer.php
+++ dev/wp-content/themes/csi/footer.php
@@ -32,12 +32,21 @@
 		<div class="footer__news--text">
 			<?php echo get_field('intro_newsletter', 'option'); ?>
 		</div>
-		<?php echo get_field('formulaire', 'option'); ?>
-		<!-- <form action="" method="POST" class="footer__form">
-			<label for="email" class="sro">Votre email</label>
-			<input type="email" class="footer__form--input" name="email" id="email" placeholder="ex.: jean.dupont@mail.com" />
-			<input type="button" class="footer__form--button" value="OK" />
-		</form> -->
+		<!-- Begin MailChimp Signup Form -->
+		<div id="mc_embed_signup">
+		<form class="footer__form" action="//anne-remacle.us15.list-manage.com/subscribe/post?u=06e8994cfe5cc4d170a29846a&amp;id=45f540f195" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+		    <div id="mc_embed_signup_scroll">
+			<label for="mce-EMAIL" class="footer__label">Email</label>
+			<input type="email" value="" name="EMAIL" class="email footer__input" id="mce-EMAIL" placeholder="ex: jean@dupont.be" required>
+		    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+
+		    <div class="clear footer__button"><input type="submit" value="OK" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+			<div aria-hidden="true"><input type="hidden" name="b_06e8994cfe5cc4d170a29846a_45f540f195" tabindex="-1" value=""></div>
+		    </div>
+		</form>
+		</div>
+
+		<!--End mc_embed_signup-->
 	</div>
 </footer>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyClfGzs3JWygOHF-J3GQqJke__tElojigQ"></script>
