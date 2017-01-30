ac7c5ac1c8c31e755a2528bfe0ca360822553ed5
Anne Remacle <anne.m.remacle@gmail.com>
2017-01-30 09:52:30 +0100

    elements organisation in footer

--- dev/wp-content/themes/csi/footer.php
+++ dev/wp-content/themes/csi/footer.php
@@ -17,37 +17,37 @@
 			'theme-location' => 'footer' ) ); ?>
 		<?php wp_reset_query(); ?>
 	</nav>
-	<div class="footer__coord coord">
-		<div class="coord__left">
+	<div class="footer__bottom">
+		<div class="footer__column coord">
 			<p class="coord__address">
 				<?php echo get_field('adresse', 'option'); ?>
 			</p>
 		</div>
-		<div class="coord__right">
+		<div class="footer__column coord">
 			<p class="coord__data">Tél&nbsp;: <?php echo get_field('telephone', 'option'); ?></p>
 			<p class="coord__data">Email&nbsp;: <?php echo get_field('email', 'option'); ?></p>
 		</div>
-	</div>
-	<div class="footer__news">
-		<div class="footer__news--text">
-			<?php echo get_field('intro_newsletter', 'option'); ?>
-		</div>
-		<!-- Begin MailChimp Signup Form -->
-		<div id="mc_embed_signup">
-		<form class="footer__form" action="//anne-remacle.us15.list-manage.com/subscribe/post?u=06e8994cfe5cc4d170a29846a&amp;id=45f540f195" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-		    <div id="mc_embed_signup_scroll">
-			<label for="mce-EMAIL" class="footer__label">Email</label>
-			<input type="email" value="" name="EMAIL" class="email footer__input" id="mce-EMAIL" placeholder="ex: jean@dupont.be" required>
-		    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+		<div class="footer__column footer__news">
+			<div class="footer__news--text">
+				<?php echo get_field('intro_newsletter', 'option'); ?>
+			</div>
+			<!-- Begin MailChimp Signup Form -->
+			<div id="mc_embed_signup">
+				<form class="footer__form" action="//anne-remacle.us15.list-manage.com/subscribe/post?u=06e8994cfe5cc4d170a29846a&amp;id=45f540f195" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				    <div id="mc_embed_signup_scroll">
+					<label for="mce-EMAIL" class="footer__label">Email</label>
+					<input type="email" value="" name="EMAIL" class="email footer__input" id="mce-EMAIL" placeholder="ex: jean@dupont.be" required>
+				    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 
-		    <div class="clear footer__button"><input type="submit" value="OK" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-			<div aria-hidden="true"><input type="hidden" name="b_06e8994cfe5cc4d170a29846a_45f540f195" tabindex="-1" value=""></div>
-		    </div>
-		</form>
+				    <div class="clear footer__button"><input type="submit" value="OK" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+					<div aria-hidden="true"><input type="hidden" name="b_06e8994cfe5cc4d170a29846a_45f540f195" tabindex="-1" value=""></div>
+				    </div>
+				</form>
+			</div>
+			<!--End mc_embed_signup-->
 		</div>
-
-		<!--End mc_embed_signup-->
 	</div>
+
 </footer>
 <div class="content__hide"></div>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyClfGzs3JWygOHF-J3GQqJke__tElojigQ"></script>
